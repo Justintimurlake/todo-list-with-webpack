4d0e493dc54f6374d3508f9055f586e2e00795ae
Justintimurlake <adamovtimur98@gmail.com>
2022-08-14 19:32:28 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,58 @@
+import style from './main.css';
+// import component from './component.js';
+
+// console.log('¡Hola!');
+// console.log('adios');
+
+// document.body.append(component());
+
+ const addTask = document.getElementById('add-task');
+ const taskContainer = document.getElementById('task-container');
+ const inputTask = document.getElementById('input-task');
+
+ inputTask.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      addTask.click();
+    }
+  });
+
+ 
+ addTask.addEventListener('click', function() {
+    let task = document.createElement('div');
+    task.classList.add('task');
+
+    
+
+    let li = document.createElement('li');
+    li.innerText = `${inputTask.value}`;
+    task.appendChild(li);
+
+    let checkButton = document.createElement('button');
+    checkButton.innerHTML = '✅';
+    checkButton.classList.add('checkTask');
+    task.appendChild(checkButton);
+
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '🗑️';
+    deleteButton.classList.add('deleteTask');
+    task.appendChild(deleteButton);
+
+    if(inputTask.value === '') {
+        alert('Please Enter Something!');
+    } else {
+        taskContainer.appendChild(task);
+    }
+
+    inputTask.value = '';
+
+
+    checkButton.addEventListener('click', function(){
+        checkButton.parentElement.style.textDecoration = 'line-through';
+    });
+    deleteButton.addEventListener('click', function(e){
+        let target = e.target;
+        target.parentElement.remove();
+    
+    });
+ });
